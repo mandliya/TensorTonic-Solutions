2062cf151c0c07ca6eee02bffa13240d50fe404e
Ravi Mandliya <ravi.mandliya@gmail.com>
2026-05-12 15:12:30 -0700

    Update solution for pad-sequences

--- pad-sequences/pad-sequences.py
+++ pad-sequences/pad-sequences.py
@@ -7,11 +7,9 @@ def pad_sequences(seqs, pad_value=0, max_len=None):
       L = max_len if provided else max(len(seq) for seq in seqs) or 0
     """
     N = len(seqs)
-    L = max_len if max_len is not None else max((len(s) for s in seqs), default=0)
+    L = max_len if max_len is not None else max((len(s) for s in seqs), default = 0)
     result = np.full((N, L), pad_value)
     for i, seq in enumerate(seqs):
-        length = min(len(seq), L)
-        result[i, :length] =  seq[:length]
-
+        n = min(len(seq), L)
+        result[i, :n] = seq[:n]
     return result
-    
