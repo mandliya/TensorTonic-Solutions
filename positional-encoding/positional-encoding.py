138ef78e808765fe1d96db346236958299360781
Ravi Mandliya <ravi.mandliya@gmail.com>
2026-05-12 15:55:24 -0700

    Update solution for positional-encoding

--- positional-encoding/positional-encoding.py
+++ positional-encoding/positional-encoding.py
@@ -6,10 +6,11 @@ def positional_encoding(seq_len, d_model, base=10000.0):
     Odd d_model -> last column is sin.
     """
     pe = np.zeros((seq_len, d_model))
-    positions = np.arange(seq_len).reshape(-1, 1)  #[seq_len, 1]
-    two_is = np.arange(0, d_model, 2) 
-    div_term = np.power(base, two_is/d_model)
-    angles = positions / div_term
+    positions = np.arange(0, seq_len).reshape(-1, 1) #[seq_len, 1]
+    two_is = np.arange(0, d_model, 2)
+    inv_freq = np.exp(-two_is/d_model * np.log(base))
+    angles = positions * inv_freq
     pe[:, 0::2] = np.sin(angles)
     pe[:, 1::2] = np.cos(angles[:, :d_model//2])
     return pe
+    
